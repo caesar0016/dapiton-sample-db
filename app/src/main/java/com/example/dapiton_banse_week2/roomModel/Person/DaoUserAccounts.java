150e088fb0ece6d1c4c51449d2e2c81822884b41
caesar.dapiton <dapitoncaesar26@gmail.com>
2025-03-20 16:09:08 +0800

    user accounts update

---
+++ app/src/main/java/com/example/dapiton_banse_week2/roomModel/Person/DaoUserAccounts.java
@@ -0,0 +1,33 @@
+package com.example.dapiton_banse_week2.roomModel.Person;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import java.util.List;
+
+@Dao
+public interface DaoUserAccounts {
+
+    @Insert
+    void insertAccount(UserAccounts user);
+
+    @Query("Select * from UserAccounts")
+    List<UserAccounts> getAllAccounts();
+
+    //--- list all the accounts except the user_id who's currently login
+    @Query("Select * from UserAccounts where user_id != :userID")
+    List<UserAccounts> getAllAccounts(int userID);
+
+    @Query("Select * from UserAccounts where user_id = :user_id Limit 1")
+    UserAccounts getUserID(int user_id);
+
+    @Update
+    void updateAccount(UserAccounts user);
+
+    @Delete
+    void deleteAccount(UserAccounts user);
+
+}
---
+++ app/src/main/java/com/example/dapiton_banse_week2/roomModel/Person/UserAccounts.java
@@ -0,0 +1,58 @@
+package com.example.dapiton_banse_week2.roomModel.Person;
+
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity
+public class UserAccounts {
+    @PrimaryKey(autoGenerate = true)
+    private int user_id;
+    private String username;
+    private String password;
+    private String userType = "Guest";
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUserType() {
+        return userType;
+    }
+
+    public void setUserType(String userType) {
+        this.userType = userType;
+    }
+
+    public UserAccounts(int user_id, String username, String password, String userType) {
+        this.user_id = user_id;
+        this.username = username;
+        this.password = password;
+        this.userType = userType;
+    }
+
+    public UserAccounts(String username, String password, String userType) {
+        this.username = username;
+        this.password = password;
+        this.userType = userType;
+    }
+}
